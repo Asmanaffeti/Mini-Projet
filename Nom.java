ecc255a76128eb01c32e519c21cc0a8c4131b1e9
eya joudi <262185128+eyajoudi@users.noreply.github.com>
2026-05-13 13:49:59 +0100

    Update Nom.java

--- Nom.java
+++ Nom.java
@@ -1,62 +1,50 @@
+package pepsmatcher.core;
+
 public class Nom {
-        private String nomOriginal;
-        private List<string> nomNormalise;
+    private String nomOriginal;
+    private String nomNormalise;
 
-        public Nom(String nomComplet) {
-            this.nomOriginal = nomComplet;
-        }
-        public int getNbMots() {
-            int count = 0;
-            boolean dansMot = false;
-            for (int i = 0; i < nomOriginal.length(); i++) {
-                char c = nomOriginal.charAt(i);
+    public Nom(String nomOriginal) {
+        this.nomOriginal = nomOriginal;
+        this.nomNormalise = nomOriginal;
+    }
 
-                if (c != ' ') {
-                    if (!dansMot) {
-                        count++;
-                        dansMot = true;
-                    }
-                } else {
-                    dansMot = false;
-                }
-            }
-            return count;
-        }
-        public int getLongueurTotal() {
-            int longueur = 0;
+    public Nom(String nomOriginal, String nomNormalise) {
+        this.nomOriginal = nomOriginal;
+        this.nomNormalise = nomNormalise;
+    }
 
-            for (int i = 0; i < nomOriginal.length(); i++) {
-                if (nom_original.charAt(i) != ' ') {
-                    longueur++;
-                }
-            }
-            return longueur;
-        }
-        public String getPremierMot() {
-            String resultat = "";
-            int i = 0;
-            while (i < nomOriginal.length() && nomOriginal.charAt(i) == ' ') {
-                i++;
-            }
-            while (i < nomOriginal.length() && nomOriginal.charAt(i) != ' ') {
-                resultat += nomOriginal.charAt(i);
-                i++;
-            }
+    public String getNomOriginal() {
+        return nomOriginal;
+    }
 
-            return resultat;
-        }
-        public String getDernierMot() {
-            String resultat = "";
-            int i = nomOriginal.length() - 1;
-            while (i >= 0 && nomOriginal.charAt(i) == ' ') {
-                i--;
-            }
-            while (i >= 0 && nomOriginal.charAt(i) != ' ') {
-                resultat = nom_original.charAt(i) + resultat;
-                i--;
-            }
+    public String getNomNormalise() {
+        return nomNormalise;
+    }
 
-            return resultat;
-        }
+    public int getNbMots() {
+        return nomNormalise.trim().isEmpty() ? 0 : nomNormalise.trim().split("\\s+").length;
+    }
+
+    public int getLongueurTotale() {
+        return nomNormalise.length();
+    }
+
+    public String[] getMots() {
+        return nomNormalise.trim().split("\\s+");
+    }
+
+    public String getPremierMot() {
+        String[] mots = getMots();
+        return mots.length > 0 ? mots[0] : "";
+    }
+
+    public String getDernierMot() {
+        String[] mots = getMots();
+        return mots.length > 0 ? mots[mots.length - 1] : "";
+    }
+
+    public String toString() {
+        return nomOriginal;
     }
 }
