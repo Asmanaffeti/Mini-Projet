24099e0c2a1e24feb57fcf7db745a3bc0bf9e22c
mariemelabed-design <mariem.elabed@etudiant-enit.utm.tn>
2026-05-14 21:57:41 +0100

    Suppression fichiers livreurs Nom README ResultatMatch

--- LivreurResultat.java
+++
@@ -1,8 +0,0 @@
-package pepsmatcher.livreur;
-
-import pepsmatcher.core.ResultatMatch;
-
-public interface LivreurResultat {
-    void livrer(ResultatMatch r);
-    void setResultat(ResultatMatch r);
-}
